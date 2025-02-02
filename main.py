7cd4a6e6dca8f847bbe806ba4a655872c825de20
Adamspliff <rk2346@posteo.de>
2025-02-02 22:07:06 +0100

    B: black screen with white triangle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -9,7 +10,10 @@ def main():
     print("Screen height: 720")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-   
+    
+    clock = pygame.time.Clock()
+    dt=0
+    
     while True:
 
         for event in pygame.event.get():
@@ -17,9 +21,12 @@ def main():
                 return
         
         screen.fill((0,0,0))
+        
+        player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+        player.draw(screen)
 
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
        
 if __name__ == "__main__":
     main()
