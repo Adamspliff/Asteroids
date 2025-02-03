be8bc66754de4e2eb2f99029ffbe7539a6aabd04
Adamspliff <rk2346@posteo.de>
2025-02-03 09:23:20 +0100

    C: player triangle with movement

--- main.py
+++ main.py
@@ -14,6 +14,11 @@ def main():
     clock = pygame.time.Clock()
     dt=0
     
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
     while True:
 
         for event in pygame.event.get():
@@ -22,11 +27,18 @@ def main():
         
         screen.fill((0,0,0))
         
-        player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
-        player.draw(screen)
+        updatable.update(dt)
+
+        for i in drawable:
+            i.draw(screen)
+        #drawable.draw(screen) would work (pygame draws everything with sprite.image and sprite.rect)
+
+        
+        
 
         pygame.display.flip()
         dt = clock.tick(60) / 1000
+
        
 if __name__ == "__main__":
     main()
